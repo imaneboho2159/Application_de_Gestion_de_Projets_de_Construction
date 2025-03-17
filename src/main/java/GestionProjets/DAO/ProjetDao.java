d699f9b88193847666edcdf4eee1ca46bd3763b5
imaneboho2159 <imaboh21@gmail.com>
2025-03-17 13:54:52 +0100

    ajouter le creud dans le package dao

--- src/main/java/GestionProjets/DAO/ProjetDao.java
+++ src/main/java/GestionProjets/DAO/ProjetDao.java
@@ -1,4 +1,98 @@
 package GestionProjets.DAO;
 
+import GestionProjets.Model.Projet;
+import Util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjetDao {
+
+
+public void AjouterProjet(Projet projet) {
+    String query= "insert into projets values(?,?,?,?,?)";
+    try (Connection connection = DBConnection.getConnection();
+    PreparedStatement preparedStatement = connection.prepareStatement(query)){
+        preparedStatement.setString(1,projet.getNom());
+        preparedStatement.setString(2,projet.getDescription());
+        preparedStatement.setString(3, String.valueOf(projet.getDate_de_Début()));
+        preparedStatement.setString(4, String.valueOf(projet.getDate_de_Fin()));
+        preparedStatement.setDouble(5,projet.getBudget());
+        preparedStatement.executeUpdate();
+
+
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+public  Projet getProjetById(int id) throws SQLException {
+    Projet projet = null;
+    String query = "select * from projet where id =?";
+    try (Connection connection = DBConnection.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()){
+            projet= new Projet(resultSet.getInt("id_projet"),
+                    resultSet.getString("nom"),
+                    resultSet.getString("description "),
+                    resultSet.getDate("Date_de_Début"),
+                    resultSet.getDate("Date_de_Fin"),
+                    resultSet.getDouble("Budget"));
+
+        }
+
+    }
+    return projet;
+}
+public List<Projet> getProjetList() throws SQLException {
+    List<Projet> projets = new ArrayList<>();
+    String query ="select * from projet";
+    try(Connection connection = DBConnection.getConnection();
+    PreparedStatement preparedStatement = connection.prepareStatement(query)){
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+           Projet projet= new Projet( resultSet.getInt("id_projet"),
+            resultSet.getString("nom"),
+            resultSet.getString("description"),
+            resultSet.getDate("Date_de_Début"),
+            resultSet.getDate("Date_de_Fin"),
+            resultSet.getDouble("Budget"));
+          projets.add(projet);
+
+        }
+    }
+return projets;
+}
+
+public void updateProjet(Projet projet) throws SQLException {
+    String query = "update projet set nom=?,description=?,Date_de_Début?,Date_de_Fin?,Budget=? where id_projet=?";
+    try(Connection connection = DBConnection.getConnection();
+    PreparedStatement preparedStatement= connection.prepareStatement(query);){
+        preparedStatement.setString(1,projet.getNom());
+        preparedStatement.setString(2,projet.getDescription());
+        preparedStatement.setDate(3,projet.getDate_de_Début());
+        preparedStatement.setDate(4,projet.getDate_de_Fin());
+        preparedStatement.setDouble(5,projet.getBudget());
+        preparedStatement.executeUpdate();
+
+    }
+}
+
+public void deleteProjet(int id) throws SQLException {
+    String query="delete from projet where id_projet =?";
+    try(Connection connection = DBConnection.getConnection();
+    PreparedStatement preparedStatement= connection.prepareStatement(query)){
+        preparedStatement.setInt(1,id);
+        preparedStatement.executeUpdate();
+
+    }
+}
+
 }
--- src/main/java/GestionProjets/Model/Projet.java
+++ src/main/java/GestionProjets/Model/Projet.java
@@ -1,4 +1,80 @@
 package GestionProjets.Model;
 
+import java.util.Date;
+
 public class Projet {
+    private int id_projet;
+    private String Nom;
+    private String Description;
+    private Date Date_de_Début;
+    private Date Date_de_Fin;
+    private  Double Budget;
+
+    public Projet(int id_projet, String nom, String description, Date date_de_Début, Date date_de_Fin, Double budget) {
+        this.id_projet = id_projet;
+        Nom = nom;
+        Description = description;
+        Date_de_Début = date_de_Début;
+        Date_de_Fin = date_de_Fin;
+        Budget = budget;
+    }
+
+    public Projet(String nom, String description, Date date_de_Début, Date date_de_Fin, Double budget) {
+        Nom = nom;
+        Description = description;
+        Date_de_Début = date_de_Début;
+        Date_de_Fin = date_de_Fin;
+        Budget = budget;
+    }
+
+    public Projet() {
+    }
+
+    public int getId_projet() {
+        return id_projet;
+    }
+
+    public void setId_projet(int id_projet) {
+        this.id_projet = id_projet;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public java.sql.Date getDate_de_Début() {
+        return Date_de_Début;
+    }
+
+    public void setDate_de_Début(Date date_de_Début) {
+        Date_de_Début = date_de_Début;
+    }
+
+    public java.sql.Date getDate_de_Fin() {
+        return Date_de_Fin;
+    }
+
+    public void setDate_de_Fin(Date date_de_Fin) {
+        Date_de_Fin = date_de_Fin;
+    }
+
+    public Double getBudget() {
+        return Budget;
+    }
+
+    public void setBudget(Double budget) {
+        Budget = budget;
+    }
 }
